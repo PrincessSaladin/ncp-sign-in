3459c8814221f20cceb0dc55d984dff609182a2d
chs <13507008@qq.com>
2020-02-15 00:36:35 +0800

    :bug: 修改表单填写方式，利用自动加载旧数据填写表单

--- auto_signed.py
+++ auto_signed.py
@@ -2,6 +2,9 @@
 from selenium import webdriver
 import schedule
 import time
+import logging
+logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',level=logging.DEBUG)
+
 
 def sign_in():
     # windows
@@ -19,25 +22,16 @@ def sign_in():
     driver.find_element_by_id("password").send_keys("密码")
     driver.find_element_by_class_name("btn-submit").click()
 
-    # 表单填充代码，修改所在城市（浏览器中执行js）
-    # driver.execute_script("document.getElementsByName('mqszd')[0].value=\"浙江省 衢州市 龙游县\";\n" +
-    #                     "document.getElementsByName('sfjcs1')[1].checked=true;\n" +
-    #                     "document.getElementsByName('sfjcs2')[1].checked=true;\n" +
-    #                     "document.getElementsByName('sfdgyq')[1].checked=true;\n" +
-    #                     "document.getElementsByName('sfcyyxgc')[1].checked=true;\n" +
-    #                     "document.getElementsByName('sfcyjjgl')[1].checked=true;\n" +
-    #                     "document.getElementsByName('xstzk')[1].checked=true;\n" +
-    #                     "document.getElementsByName('jssfjcs1')[1].checked=true;\n" +
-    #                     "document.getElementsByName('jssfjcs2')[1].checked=true;\n" +
-    #                     "document.getElementsByName('jssfdgyq')[1].checked=true;\n" +
-    #                     "document.getElementsByName('jssfcyyxgc')[1].checked=true;")
-
+    # 表单填充代码，修改所在城市
+    driver.execute_script("localStorage.setItem(\"lastSubmit-2\",\"{\\\"tbrq\\\":\\\"2020-02-14\\\",\\\"mqszd\\\":\\\"浙江省 衢州市 龙游县\\\",\\\"sfjcs1\\\":\\\"否\\\",\\\"sfjcs2\\\":\\\"否\\\",\\\"sfdgyq\\\":\\\"否\\\",\\\"sfcyyxgc\\\":\\\"否\\\",\\\"yxgcxx\\\":\\\"\\\",\\\"sfcyjjgl\\\":\\\"否\\\",\\\"jjglxx\\\":\\\"\\\",\\\"xstzk\\\":\\\"否\\\",\\\"jssfjcs1\\\":\\\"否\\\",\\\"jssfjcs2\\\":\\\"否\\\",\\\"jssfdgyq\\\":\\\"否\\\",\\\"jssfcyyxgc\\\":\\\"否\\\"}\")")
+    driver.refresh()
     # 提交（第一次运行无误后取消下方代码注释）
     driver.find_element_by_class_name("btn-commit").click()
     driver.quit()
 
 
 if __name__ == '__main__':
+    logging.info("Application Start")
     schedule.every().day.at("00:10").do(sign_in)
     while True:
         schedule.run_pending()
