423c4a50b851722c3d658709d9167ace7246a1fa
chs <13507008@qq.com>
2020-02-16 00:12:14 +0800

    :bug: 预留浏览器js加载页面时间

--- auto_signed.py
+++ auto_signed.py
@@ -25,6 +25,7 @@ def sign_in():
     # 表单填充代码，修改所在城市
     driver.execute_script("localStorage.setItem(\"lastSubmit-2\",\"{\\\"tbrq\\\":\\\"2020-02-14\\\",\\\"mqszd\\\":\\\"浙江省 衢州市 龙游县\\\",\\\"sfjcs1\\\":\\\"否\\\",\\\"sfjcs2\\\":\\\"否\\\",\\\"sfdgyq\\\":\\\"否\\\",\\\"sfcyyxgc\\\":\\\"否\\\",\\\"yxgcxx\\\":\\\"\\\",\\\"sfcyjjgl\\\":\\\"否\\\",\\\"jjglxx\\\":\\\"\\\",\\\"xstzk\\\":\\\"否\\\",\\\"jssfjcs1\\\":\\\"否\\\",\\\"jssfjcs2\\\":\\\"否\\\",\\\"jssfdgyq\\\":\\\"否\\\",\\\"jssfcyyxgc\\\":\\\"否\\\"}\")")
     driver.refresh()
+    time.sleep(10)
     # 提交（第一次运行无误后取消下方代码注释）
     driver.find_element_by_class_name("btn-commit").click()
     driver.quit()
