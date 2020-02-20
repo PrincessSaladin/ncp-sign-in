6b33d464a18222aa330c67eae8c54d48ec04ffb4
chs <13507008@qq.com>
2020-02-21 00:06:34 +0800

    :sparkles: 更新key指为最新

--- signed.py
+++ signed.py
@@ -19,7 +19,7 @@ driver.find_element_by_id("password").send_keys("密码")
 driver.find_element_by_class_name("btn-submit").click()
 
 # 表单填充代码，修改所在城市（浏览器中执行js）
-driver.execute_script("localStorage.setItem(\"lastSubmit-2\",\"{\\\"tbrq\\\":\\\"2020-02-17\\\",\\\"mqszd\\\":\\\"浙江省 衢州市 龙游县\\\",\\\"sfjcs1\\\":\\\"否\\\",\\\"sfjcs2\\\":\\\"否\\\",\\\"sfdgyq\\\":\\\"否\\\",\\\"sfcyyxgc\\\":\\\"否\\\",\\\"yxgcxx\\\":\\\"\\\",\\\"sfcyjjgl\\\":\\\"否\\\",\\\"jjglxx\\\":\\\"\\\",\\\"xstzk\\\":\\\"否\\\",\\\"jssfjcs1\\\":\\\"否\\\",\\\"jssfjcs2\\\":\\\"否\\\",\\\"jssfdgyq\\\":\\\"否\\\",\\\"jssfcyyxgc\\\":\\\"否\\\",\\\"hzjkm\\\":\\\"绿码\\\"}\")")
+driver.execute_script("localStorage.setItem(\"lastSubmit-2\",\"{\\\"tbrq\\\":\\\"2020-02-21\\\",\\\"mqszd\\\":\\\"浙江省 衢州市 龙游县\\\",\\\"sfjcs110\\\":\\\"否\\\",\\\"sfjcs2\\\":\\\"否\\\",\\\"sfdgyq0\\\":\\\"否\\\",\\\"sfcyyxgc\\\":\\\"否\\\",\\\"yxgcxx\\\":\\\"\\\",\\\"sfcyjjgl\\\":\\\"否\\\",\\\"jjglxx\\\":\\\"\\\",\\\"xstzk\\\":\\\"否\\\",\\\"jssfjcs10\\\":\\\"否\\\",\\\"jssfjcs2\\\":\\\"否\\\",\\\"jssfdgyq\\\":\\\"否\\\",\\\"jssfcyyxgc\\\":\\\"否\\\",\\\"hzjkm\\\":\\\"绿码\\\"}\")")
 driver.refresh()
 
 # 提交（第一次运行无误后取消下方代码注释）
